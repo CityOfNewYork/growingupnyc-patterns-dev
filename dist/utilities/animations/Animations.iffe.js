a248a7196a5a78825ebf597bfce78013ea98e1a7
kimpenguin <kpeng@nycopportunity.nyc.gov>
2020-07-14 10:59:24 -0400

    adding fade functions for animations utility

--- dist/utilities/animations/Animations.iffe.js
+++ dist/utilities/animations/Animations.iffe.js
@@ -936,7 +936,8 @@ var Animations = (function () {
   var Animations = function Animations() {
     this._settings = {
       selector: Animations.selector,
-      controller: Animations.controller
+      controller: Animations.controller,
+      speed: Animations.speed
     };
     var rotating = document.querySelectorAll(this._settings.selector);
     var terms = [];
@@ -945,58 +946,107 @@ var Animations = (function () {
         terms.push(term.innerText);
       }
     });
-    this.rotateTerm(terms);
+    Animations.rotateTerms(terms, this._settings.controller, this._settings.speed);
   };
+  /**
+   * Rotates strings
+   * @param {*} terms 
+   * @param {*} control 
+   * @param {*} speed 
+   */
 
-  Animations.prototype.rotateTerm = function rotateTerm(terms) {
-    var controller = document.querySelector(this._settings.controller);
-    Animations.fadein(controller); // console.log(terms[0]);
 
+  Animations.rotateTerms = function (terms, control, speed) {
+    var controller = document.querySelector(control);
     controller.innerText = terms[0].trim();
     var i = 0;
     setInterval(function () {
-      controller.style.opacity = 1;
-
       if (i == terms.length) {
         i = 0;
       }
 
       controller.innerText = terms[i].trim();
-      Animations.fadeout(controller); // Animations.fadeout(controller)
-      // var fadeEffect = setInterval(function () {
-      // if (controller.style.opacity > 0) {
-      //   controller.style.opacity -= 0.1;
-      // } else {
-      //   clearInterval(fadeEffect)
-      // }
-      // }, 100);
+
+      if (!controller.style.opacity) {
+        Animations.fadeOut(controller, speed);
+      } else {
+        Animations.fadeInOut(controller, speed);
+      }
 
       i++;
     }, 3000);
   };
+  /**
+   * Fades out element
+   * Reference: https://stackoverflow.com/questions/28662893/fade-in-and-fade-out-in-pure-javascript-without-jquery
+   * @param {*} elem the element
+   * @param {*} speed the speed of the interval
+   */
 
-  Animations.fadeout = function (cat) {
-    console.log('fadeout');
-    var fdout = setInterval(function () {
-      if (parseFloat(cat.style.opacity) > 0) {
-        cat.style.opacity = parseFloat(cat.style.opacity) - 0.01;
-      } else {
-        clearInterval(fdout);
+
+  Animations.fadeOut = function (elem, speed) {
+    if (!elem.style.opacity) {
+      elem.style.opacity = 1;
+    }
+
+    var outInterval = setInterval(function () {
+      elem.style.opacity -= 0.02;
+
+      if (elem.style.opacity <= 0) {
+        clearInterval(outInterval);
       }
-    }, 10);
+    }, speed / 50);
+  };
+  /**
+   * Fades in element
+   * Reference: https://stackoverflow.com/questions/28662893/fade-in-and-fade-out-in-pure-javascript-without-jquery
+   * @param {*} elem the element
+   * @param {*} speed the speed of the interval
+   */
+
+
+  Animations.fadeIn = function (elem, speed) {
+    if (!elem.style.opacity) {
+      elem.style.opacity = 0;
+    }
+
+    var inInterval = setInterval(function () {
+      elem.style.opacity = Number(elem.style.opacity) + 0.02;
+
+      if (elem.style.opacity >= 1) {
+        clearInterval(inInterval);
+      }
+    }, speed / 50);
+  };
+  /**
+   * Fades in and out element
+   * @param {*} elem the element
+   * @param {*} speed the speed of the interval
+   */
+
+
+  Animations.fadeInOut = function (elem, speed) {
+    if (!elem.style.opacity) {
+      elem.style.opacity = 0;
+    }
+
+    var inInterval = setInterval(function () {
+      elem.style.opacity = Number(elem.style.opacity) + 0.02;
+
+      if (elem.style.opacity >= 1) {
+        clearInterval(inInterval);
+        var outInterval = setInterval(function () {
+          elem.style.opacity -= 0.02;
+
+          if (elem.style.opacity <= 0) {
+            clearInterval(outInterval);
+          }
+        }, speed / 50);
+      }
+    }, speed / 50);
   };
 
-  Animations.fadein = function (cat) {
-    console.log('fadin');
-    var fdin = setInterval(function () {
-      if (parseFloat(cat.style.opacity) < 1) {
-        cat.style.opacity = parseFloat(cat.style.opacity) + 0.01;
-      } else {
-        clearInterval(fdin);
-      }
-    }, 10);
-  };
-
+  Animations.speed = 1500;
   Animations.selector = '[data-js*="rotate-text"]';
   Animations.controller = '[data-js*="rotate-controller"]';
 
--- src/utilities/animations/Animations.js
+++ src/utilities/animations/Animations.js
@@ -19,6 +19,7 @@ class Animations {
     this._settings = {
       selector: Animations.selector,
       controller: Animations.controller,
+      speed: Animations.speed,
     };
 
     const rotating = document.querySelectorAll(this._settings.selector);
@@ -30,61 +31,100 @@ class Animations {
       }
     })
 
-    this.rotateTerm(terms)
-  }
-
-  rotateTerm(terms) {
-    const controller = document.querySelector(this._settings.controller)
-    Animations.fadein(controller)
-
-		// console.log(terms[0]);
-
-    controller.innerText = terms[0].trim();
-    var i = 0;
-    setInterval(function () {
-      controller.style.opacity = 1;
-      if (i == terms.length) {
-        i = 0;
-      }
-      controller.innerText = terms[i].trim();
-
-      Animations.fadeout(controller)
-      // Animations.fadeout(controller)
-      // var fadeEffect = setInterval(function () {
-      //   if (controller.style.opacity > 0) {
-      //     controller.style.opacity -= 0.1;
-      //   } else {
-      //     clearInterval(fadeEffect)
-      //   }
-      // }, 100);
-
-      i++;
-    }, 3000);
+    Animations.rotateTerms(terms, this._settings.controller, this._settings.speed)
   }
 }
 
-Animations.fadeout = function(cat) {
-  console.log('fadeout')
-  let fdout = setInterval(function () {
-    if (parseFloat(cat.style.opacity) > 0) {
-      cat.style.opacity = parseFloat(cat.style.opacity) - 0.01;
-    } else {
-      clearInterval(fdout);
+/**
+ * Rotates strings
+ * @param {*} terms 
+ * @param {*} control 
+ * @param {*} speed 
+ */
+Animations.rotateTerms = function(terms, control, speed) {
+  const controller = document.querySelector(control)
+
+  controller.innerText = terms[0].trim();
+
+  var i = 0;
+  setInterval(function () {
+    if (i == terms.length) {
+      i = 0;
     }
-  }, 10);
+    controller.innerText = terms[i].trim();
+
+    if (!controller.style.opacity) {
+      Animations.fadeOut(controller, speed)
+    } else {
+      Animations.fadeInOut(controller, speed);
+
+    }
+    i++;
+  }, 3000);
 }
 
+/**
+ * Fades out element
+ * Reference: https://stackoverflow.com/questions/28662893/fade-in-and-fade-out-in-pure-javascript-without-jquery
+ * @param {*} elem the element
+ * @param {*} speed the speed of the interval
+ */
+Animations.fadeOut = function(elem, speed) {
+  if (!elem.style.opacity) {
+    elem.style.opacity = 1;
+  }
 
-Animations.fadein = function(cat) {
-  console.log('fadin')
-  let fdin = setInterval(function () {
-    if (parseFloat(cat.style.opacity) < 1) {
-      cat.style.opacity = parseFloat(cat.style.opacity) + 0.01;
-    } else {
-      clearInterval(fdin);
+  var outInterval = setInterval(function () {
+    elem.style.opacity -= 0.02;
+    if (elem.style.opacity <= 0) {
+      clearInterval(outInterval);
     }
-  }, 10);
+  }, speed / 50);
+} 
+
+/**
+ * Fades in element
+ * Reference: https://stackoverflow.com/questions/28662893/fade-in-and-fade-out-in-pure-javascript-without-jquery
+ * @param {*} elem the element
+ * @param {*} speed the speed of the interval
+ */
+Animations.fadeIn = function (elem, speed) {
+  if (!elem.style.opacity) {
+    elem.style.opacity = 0;
+  }
+
+  var inInterval = setInterval(function () {
+    elem.style.opacity = Number(elem.style.opacity) + 0.02;
+    if (elem.style.opacity >= 1)
+      clearInterval(inInterval);
+  }, speed / 50);
 }
+
+/**
+ * Fades in and out element
+ * @param {*} elem the element
+ * @param {*} speed the speed of the interval
+ */
+Animations.fadeInOut = function (elem, speed) {
+  if (!elem.style.opacity) {
+    elem.style.opacity = 0;
+  }
+
+  var inInterval = setInterval(function () {
+    elem.style.opacity = Number(elem.style.opacity) + 0.02;
+    if (elem.style.opacity >= 1){
+      clearInterval(inInterval);
+      var outInterval = setInterval(function () {
+        elem.style.opacity -= 0.02;
+        if (elem.style.opacity <= 0) {
+          clearInterval(outInterval);
+        }
+      }, speed / 50);
+    }
+  }, speed / 50);
+}
+
+Animations.speed = 1500;
 
 Animations.selector = '[data-js*="rotate-text"]';
 
